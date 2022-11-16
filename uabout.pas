9e683869d3ec97d4223300d3871762d12b19aa29
Assis Dantas <62859920+assisdantas@users.noreply.github.com>
2022-11-16 12:04:23 -0300

    Version 1, TCP support
    Version 1 only works with TCP ports, as it is an initial build. I'm already implementing UDP support and I'll commit soon.

---
+++ uabout.pas
@@ -0,0 +1,31 @@
+unit uabout;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, Vcl.Imaging.pngimage,
+  Vcl.ExtCtrls;
+
+type
+  Tfrmabout = class(TForm)
+    Image1: TImage;
+    Label1: TLabel;
+    Label2: TLabel;
+    Label3: TLabel;
+    Label4: TLabel;
+    Label5: TLabel;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  frmabout: Tfrmabout;
+
+implementation
+
+{$R *.dfm}
+
+end.
